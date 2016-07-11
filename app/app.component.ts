9a199039f11c8015c3db22fff6511fbb5f25b21d
Alexander Stier <Alexander Stier>
2016-07-11 11:23:36 +0200

    Update 20160711

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>Meine zweite Angular 2 App</h1>'
+    template: '<h1>Meine erste Angular 2 App in VSCode.</h1>'
 })
 export class AppComponent { }
